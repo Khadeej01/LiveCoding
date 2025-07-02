b05566cc5cc2d8dc983f378611e8d8f7bdfdabbe
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-02 15:22:42 +0200

    modif

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
-import { AppComponent} from "./app.component";
-import {ClientsComponent} from "./component/clients/clients.component";
+import { ClientsComponent } from './clients/clients.component';
 
 export const routes: Routes = [
-    {path: '', component: AppComponent},
-    {path: 'Client', component: ClientsComponent }
-
-
+  { path:'', component:ClientsComponent}
 ];
