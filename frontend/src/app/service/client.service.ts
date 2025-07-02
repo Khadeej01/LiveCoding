623d8d181256b43771452271fda998d681a8dfe7
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-02 16:58:26 +0200

    LAST PUSH

--- frontend/src/app/service/client.service.ts
+++ frontend/src/app/service/client.service.ts
@@ -1,29 +1,38 @@
+// clients.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Client {
+  id?: number;
+  nom: string;
+  prenom: string;
+  email: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // This makes it available application-wide
 })
-export class ClientService {
+export class ClientsService {
   private apiUrl = 'http://localhost:8080/api/clients';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getClients(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  // All your CRUD methods remain the same
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.apiUrl);
   }
 
-  getClient(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  getClient(id: number): Observable<Client> {
+    return this.http.get<Client>(`${this.apiUrl}/${id}`);
   }
 
-  createClient(client: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, client);
+  createClient(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.apiUrl, client);
   }
 
-  updateClient(id: number, client: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, client);
+  updateClient(id: number, client: Client): Observable<Client> {
+    return this.http.put<Client>(`${this.apiUrl}/${id}`, client);
   }
 
   deleteClient(id: number): Observable<void> {
