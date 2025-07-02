b9abe47e35b1804720ef82295239e8251365f6ca
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-02 11:50:51 +0200

    Supprimer le frontend pour réinstallation

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClientsComponent } from './clients/clients.component';
-
-export const routes: Routes = [
-  { path: 'clients', component: ClientsComponent },
-  { path: '', redirectTo: '/clients', pathMatch: 'full' }
-];
